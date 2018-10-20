1802fc8b88969c8003abab98962072c28e3f0719
SidLin <j611062000@gmail.com>
2018-10-20 12:09:07 +0800

    pseudocode

--- Medium/300_Longest Increasing Subsequence.py
+++ Medium/300_Longest Increasing Subsequence.py
@@ -37,3 +37,53 @@ def lengthOfLIS(nums):
 
 
 print(lengthOfLIS([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
+
+"""
+Function BinarySearch:
+    
+    Pass In: array, element, lower_bound, upper_bound
+    
+    While (lower bound <= upper bound):
+        
+        median = ceiling((lower_bound + upper_bound)/2)
+        pivot = array[median]
+
+        if element == pivot:
+            It means that there is an value in array as the same as the element
+            return "N" 
+        
+        elif element < pivot:
+            hi = median - 1
+            index = median
+        
+        else:
+            lo = median + 1
+            index = lo
+    
+    Pass Out: index
+
+EndFunction
+
+
+Function lengthOfLIS:
+    
+    Pass In: array of raw data
+    
+    M <- array used to store the last element of every LIS
+    
+    For each element in array of raw data:
+        insertion_position <- calculated by function, BinarySearch
+        
+        If insertion_position != "N", then we keep finding which case should we apply
+
+            Case 1: If insertion_position = (the last position of M) + 1
+                It means that the element is greater than any other value in M.
+                Thus put the element at the tail of the array, M.
+
+            Case 2: If insertion_position != (the last position of M) + 1
+                Replace M[insertion_position] with element
+
+    return length of M
+
+End Function
+"""
