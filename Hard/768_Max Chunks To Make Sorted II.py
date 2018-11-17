cbaa63d4f094f58d48037119b60aed73edb166e5
SidLin <j611062000@gmail.com>
2018-11-17 11:48:35 +0800

    768

--- Hard/768_Max Chunks To Make Sorted II.py
+++ Hard/768_Max Chunks To Make Sorted II.py
@@ -1,3 +1,6 @@
+import bisect
+
+
 class Solution(object):
 
     def maxChunksToSorted(self, arr):
@@ -6,24 +9,27 @@ class Solution(object):
         :rtype: int
         """
 
-        max_record = [arr[0]]
-        for element in arr[1:]:
-            for count, max_num in enumerate(max_record):
-                if element < max_num:
-                    temp_max = max_record[-1]
-                    if len(max_record) > count:
-                        max_record = max_record[:count]
-                    max_record.append(max(temp_max, element))
-                    break
-                elif count == len(max_record)-1:
-                    max_record.append(element)
-                    break
+        # max_record = [arr[0]]
+        # for element in arr[1:]:
+        #     temp_max = max_record[-1]
+        #     if element >= temp_max:
+        #         max_record.append(element)
+        #         continue
 
+        #     max_record = max_record[:bisect.bisect_right(max_record,element)]
+        #     max_record.append(temp_max)
 
-        return len(max_record)
+        # return len(max_record)
+        res,total = 0,0
+        for i, j in zip(arr,sorted(arr)):
+            total+=i-j
+            if not total:
+                res+=1
+        return res
+
 
 
 if __name__ == "__main__":
-    data = [5,4,3,2,1]
+    data = [2,1,3,4,4,0]
 
     print(Solution().maxChunksToSorted(data))
